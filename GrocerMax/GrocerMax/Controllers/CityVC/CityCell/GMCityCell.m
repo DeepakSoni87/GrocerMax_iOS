3fc94c711df12a9f51b93f6397292c42ec3b72b8
Arvind Kumar Gupta <arvindgupta@Arvinds-MacBook-Pro.local>
2015-09-23 03:19:12 +0530

    Arvind : city service consume and implementation

---
+++ GrocerMax/GrocerMax/Controllers/CityVC/CityCell/GMCityCell.m
@@ -0,0 +1,23 @@
+//
+//  GMCityCell.m
+//  GrocerMax
+//
+//  Created by Arvind Kumar Gupta on 23/09/15.
+//  Copyright (c) 2015 Deepak Soni. All rights reserved.
+//
+
+#import "GMCityCell.h"
+
+@implementation GMCityCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
