4e54d8e75af71271c7286ae7bc333a2b2c9710ec
Arvind Kumar Gupta <arvindgupta@Arvinds-MacBook-Pro.local>
2015-09-26 02:51:55 +0530

    Arvind Gupta : Order Detail

--- GrocerMax/GrocerMax/Controllers/OrderHistoryDetailVC/OrderDetailCell/GMOrderItemCell.h
+++ GrocerMax/GrocerMax/Controllers/OrderHistoryDetailVC/OrderDetailCell/GMOrderItemCell.h
@@ -7,9 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GMOrderDeatilBaseModal.h"
 
 @interface GMOrderItemCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *orderItemNameLbl;
 
+@property (weak, nonatomic) IBOutlet UILabel *orderQuantityLbl;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalOrderQuantityLbl;
+
+@property (weak, nonatomic) IBOutlet UILabel *oredrItemPriceLbl;
 
 + (CGFloat)cellHeight;
+
+- (void)configerViewData:(GMOrderItemDeatilModal *)orderItemDeatilModal;
 @end
