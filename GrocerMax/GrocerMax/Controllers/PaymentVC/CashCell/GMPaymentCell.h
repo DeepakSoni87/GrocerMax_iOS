f483135d22085b64750b019e6f77e8a46a8f7b92
Arvind Kumar Gupta <arvindgupta@Arvinds-MacBook-Pro.local>
2015-09-26 23:59:39 +0530

    Arvind : COD

---
+++ GrocerMax/GrocerMax/Controllers/PaymentVC/CashCell/GMPaymentCell.h
@@ -0,0 +1,13 @@
+//
+//  GMPaymentCell.h
+//  GrocerMax
+//
+//  Created by Arvind Kumar Gupta on 26/09/15.
+//  Copyright (c) 2015 Deepak Soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMPaymentCell : UITableViewCell
+
+@end
---
+++ GrocerMax/GrocerMax/Controllers/PaymentVC/CashCell/GMPaymentCell.m
@@ -0,0 +1,23 @@
+//
+//  GMPaymentCell.m
+//  GrocerMax
+//
+//  Created by Arvind Kumar Gupta on 26/09/15.
+//  Copyright (c) 2015 Deepak Soni. All rights reserved.
+//
+
+#import "GMPaymentCell.h"
+
+@implementation GMPaymentCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
