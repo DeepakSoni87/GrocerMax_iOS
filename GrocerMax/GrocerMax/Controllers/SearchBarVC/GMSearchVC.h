a8f8d709ebcca9f13e86183a4171b68e89438246
KelltontechRahulChaudhary <rahulchaudhary90@gmail.com>
2015-10-01 04:19:24 +0530

    add search vc as 5th tab

---
+++ GrocerMax/GrocerMax/Controllers/SearchBarVC/GMSearchVC.h
@@ -0,0 +1,13 @@
+//
+//  GMSearchVC.h
+//  GrocerMax
+//
+//  Created by Rahul Chaudhary on 01/10/15.
+//  Copyright (c) 2015 Deepak Soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMSearchVC : UIViewController
+
+@end
