eae7dc753ecfe6adec4fdf44c5c47e1fe1da2e47
KelltontechRahulChaudhary <rahulchaudhary90@gmail.com>
2015-10-01 03:52:02 +0530

    change cell size on home

--- GrocerMax/GrocerMax/Controllers/ProductListingVC/GMProductListingVC.m
+++ GrocerMax/GrocerMax/Controllers/ProductListingVC/GMProductListingVC.m
@@ -37,14 +37,14 @@ NSString *const kGMProductListTableViewCell = @"GMProductListTableViewCell";
     
     self.productBaseModal = [self.rootPageAPIController.modalDic objectForKey:self.productRequestID];
     
-//    if (self.productBaseModal.productsListArray.count == 0) {
-//        [self getProducListFromServer];
-//    }
-    
     if (self.productBaseModal.productsListArray.count == 0) {
         self.productBaseModal = [[GMProductListingBaseModal alloc] init];
         self.productBaseModal.productsListArray = self.catMdl.productListArray;
         [self.rootPageAPIController.modalDic setObject:self.productBaseModal forKey:self.productRequestID];
+    }
+    
+    if (self.productBaseModal.productsListArray.count == 0) {
+        [self getProducListFromServer];
     }
     
     [self.productListTblView reloadData];
